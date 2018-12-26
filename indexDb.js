347d8258680fc742b8bd54270c51e218aa9843ef
Marco Barison <barison.marco.91@gmail.com>
2018-12-26 09:52:58 +0100

    first save in mongo

--- indexDb.js
+++ indexDb.js
@@ -9,8 +9,56 @@ userSchema = new mongoose.Schema({
     days:  [
         {
             day: String,
-            actions: Array
+            actions: [{
+                action: String,
+                isDone: { type: Boolean, default: false}
+            }]
         }
     ]
-    
 })
+
+const Checklist = mongoose.model('Checklist', userSchema)
+
+async function saveUser(){
+    const user = new Checklist({
+        user: "marco_bz"
+        lastDay : "02.01.2018",
+        days:[
+            {           
+                day: "01.01.2018" ,
+                actions: [{
+                    action: "action1",
+                    isDone: false
+                },
+                {
+                    action: "action2",
+                    isDone: false
+                },
+                {
+                    action: "action3",
+                    isDone: true
+                }]
+            },
+            {           
+                day: "02.01.2018" ,
+                actions: [{
+                    action: "action1",
+                    isDone: false
+                },
+                {
+                    action: "action2",
+                    isDone: true
+                },
+                {
+                    action: "action3",
+                    isDone: true
+                }]
+            },
+        ]
+    })
+
+    const savingUser = await user.save()
+    console.log(savingUser)
+}
+
+saveUser()
